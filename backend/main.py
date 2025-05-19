de96eca9de1b62f5fc8690d10267804fb66efc39
Anna Levchenko <annalevchenko04@users.noreply.github.com>
2025-05-19 15:31:58 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -39,6 +39,7 @@ logging.basicConfig(level=logging.DEBUG)
 origins = [
     "http://localhost:3000",
     "https://gray-coast-0457ace03.6.azurestaticapps.net",
+    "https://k548-esp-2025.onrender.com",
     "https://*.azurestaticapps.net", 
     "https://quickchart.io"
 ]
